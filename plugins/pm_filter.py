91d7c968973c51806acdba466409d4b9bfa50ce4
Jasim51511 <105831166+Jasim51511@users.noreply.github.com>
2022-06-22 12:46:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -747,8 +747,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MWUpdatez'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('💠 Ꮆ𝐫𝔬υ𝕡 💠', url='https://t.me/moviecompanyupdates'),
+            InlineKeyboardButton('💠 υק𝕕ᗩ𝓉𝐄 💠', url='https://t.me/moviecompanyupdates')
             ],[      
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
